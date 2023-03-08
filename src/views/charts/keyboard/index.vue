24f0b320af5144822edc16db3c30c6b22f7f30a8
chenlijie <chen.lijie@hxss.com.cn>
2023-03-08 15:50:07 +0800

    333

---
+++ src/views/charts/keyboard/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="keyboard">keyboard</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+  methods: {},
+  created () {}
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/charts/lines/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="lines">lines</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+  methods: {},
+  created () {}
+}
+</script>
+<style lang="scss" scoped>
+</style>
